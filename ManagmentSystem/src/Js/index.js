852fc8ef2027e6e64d065ddc2250367c25e7e84b
JeyhunMst <109238733+JeyhunMst@users.noreply.github.com>
2022-10-30 23:59:02 +0400

    Popup completed

--- ManagmentSystem/src/Js/index.js
+++ ManagmentSystem/src/Js/index.js
@@ -1,54 +1,92 @@
+//Calling elements from html
+const popup=document.querySelector("#pizzas .popup");
+
 const pizzas=[];
 class Pizza{
-    constructor(pName,ingredients,smallPrice,mediumPrice,largePrice,imgSrc){
+    constructor(pName,ingredients,smallPrice,imgSrc){
        this.pizzaName=pName;
        this.pizzaIngredients=ingredients;
        this.smallPrice=Number(smallPrice);
-       this.mediumPrice=Number(mediumPrice);
-       this.largePrice=Number(largePrice);
        this.sourceImg=imgSrc;
     }
 }
 //Pizzas
-const margherita=new Pizza("Margherita","Pizza sauce, gouda cheese, mozzarella cheese, tomatoes",7.90,11.90,14.90,"./assets/images/pizzas-section/margherita.jpg");
-const vegetarian=new Pizza("Vegetarian","Pizza sauce, gouda cheese, spinach, red onion, bell pepper, tomatoes, mushrooms",8.90,12.90,15.90,"./assets/images/pizzas-section/vegetarian.jpg");
-const funghiConPollo=new Pizza("Funghi Con Pollo","Pizza sauce, gouda cheese, mushrooms, chicken fillet",9.90,14.90,18.90,"./assets/images/pizzas-section/funghi-pollo.jpg");
-const speciale=new Pizza("Speciale","Pizza sauce, gouda cheese, chicken fillet, red onion",9.90,14.90,18.90,"./assets/images/pizzas-section/speciale.jpg");
-const mista=new Pizza("Mista","Pizza sauce, gouda cheese, mushrooms, ham (halal), salami (halal)",9.90,14.90,18.90,"./assets/images/pizzas-section/mista.jpg");
-const salami=new Pizza("Salami","Pizza sauce, gouda cheese, salami (halal)",9.90,14.90,18.90,"./assets/images/pizzas-section/salami.jpg");
-const classico=new Pizza("Classico","Pizza sauce, gouda cheese, salami (halal), mushrooms, pepperoni pepper",9.90,14.90,18.90,"./assets/images/pizzas-section/classico.jpg");
-const hawaii=new Pizza("Hawaii","Pizza sauce, gouda cheese, pineapple, ham (halal)",9.90,14.90,18.90,"./assets/images/pizzas-section/hawaii.jpg");
-const texas=new Pizza("Texas","Pizza sauce, gouda cheese, chicken fillet, bell pepper, corn, red onion",9.90,14.90,18.90,"./assets/images/pizzas-section/texas.jpg");
-const chickenStrips=new Pizza("Chicken Strips","Barbecue sauce, gouda cheese, chicken nuggets, corn, bell pepper",10.90,15.90,20.90,"./assets/images/pizzas-section/chicken-strips.jpg");
-const mexicana=new Pizza("Mexicana","Pizza sauce, gouda сheese, chicken fillet, red onion, spinach, jalapeno pepper, pepperoni",10.90,15.90,20.90,"./assets/images/pizzas-section/mexicana.jpg");
-const tonnoeCipolla=new Pizza("Tonno e Cipolla","Pizza sauce, gouda cheese, tuna, red onion",10.90,15.90,20.90,"./assets/images/pizzas-section/tonno-cipolla.jpg");
-const fiveChesees=new Pizza("Five Chesees","Creamy sauce, gouda cheese, mozzarella cheese, bufallo mozzarella cheese, riccota cheese, parmesan cheese",11.90,16.90,21.90,"./assets/images/pizzas-section/five-chesees.jpg");
-const volcano=new Pizza("Volcano","Pizza sauce, gouda cheese, spinach, red onion, bell pepper, tomatoes, mushrooms, chicken fillet, olives",11.90,16.90,21.90,"./assets/images/pizzas-section/volcano.jpg");
-const sujuk=new Pizza("Sujuk","Pizza sauce, gouda сheese, bell pepper, mushrooms, sucuk",11.90,16.90,21.90,"./assets/images/pizzas-section/sujuk.jpg");
-const amerikana=new Pizza("Amerikana","Barbecue sauce, gouda сheese, sausage, french friez, pickles",11.90,16.90,21.90,"./assets/images/pizzas-section/amerikana.jpg");
-const fourSeasons=new Pizza("Four Seasons","Meduim - 30 sm, pizza sauce, gouda cheese, white cheese, ham (halal), sucuk, mushrooms, tomatoes, oregano",16.90,16.90,16.90,"./assets/images/pizzas-section/four-season.jpg");
-const deluxe=new Pizza("Deluxe","Pizza sauce, gouda cheese, red onion, pepperoni pepper, beef",11.90,17.90,23.90,"./assets/images/pizzas-section/deluxe.jpg");
-const meatLovers=new Pizza("Meat Lovers","Pizza sauce, gouda cheese, chicken fillet, beef, salami (halal), ham (halal), sucuk",12.90,18.90,24.90,"./assets/images/pizzas-section/meat-lovers.jpg");
-const megaMix=new Pizza("Mega Mix","Pizza sauce, gouda сheese, tomatoes, bell pepper, mushrooms, black olives, salami (halal), ham (halal), beef, chicken fillet",12.90,18.90,24.90,"./assets/images/pizzas-section/mega-mix.jpg");
+const margherita=new Pizza("Margherita","Pizza sauce, gouda cheese, mozzarella cheese, tomatoes",7.90,"./assets/images/pizzas-section/margherita.jpg");
+const vegetarian=new Pizza("Vegetarian","Pizza sauce, gouda cheese, spinach, red onion, bell pepper, tomatoes, mushrooms",8.90,"./assets/images/pizzas-section/vegetarian.jpg");
+const funghiConPollo=new Pizza("Funghi Con Pollo","Pizza sauce, gouda cheese, mushrooms, chicken fillet",9.90,"./assets/images/pizzas-section/funghi-pollo.jpg");
+const speciale=new Pizza("Speciale","Pizza sauce, gouda cheese, chicken fillet, red onion",9.90,"./assets/images/pizzas-section/speciale.jpg");
+const mista=new Pizza("Mista","Pizza sauce, gouda cheese, mushrooms, ham (halal), salami (halal)",9.90,"./assets/images/pizzas-section/mista.jpg");
+const salami=new Pizza("Salami","Pizza sauce, gouda cheese, salami (halal)",9.90,"./assets/images/pizzas-section/salami.jpg");
+const classico=new Pizza("Classico","Pizza sauce, gouda cheese, salami (halal), mushrooms, pepperoni pepper",9.90,"./assets/images/pizzas-section/classico.jpg");
+const hawaii=new Pizza("Hawaii","Pizza sauce, gouda cheese, pineapple, ham (halal)",9.90,"./assets/images/pizzas-section/hawaii.jpg");
+const texas=new Pizza("Texas","Pizza sauce, gouda cheese, chicken fillet, bell pepper, corn, red onion",9.90,"./assets/images/pizzas-section/texas.jpg");
+const chickenStrips=new Pizza("Chicken Strips","Barbecue sauce, gouda cheese, chicken nuggets, corn, bell pepper",10.90,"./assets/images/pizzas-section/chicken-strips.jpg");
+const mexicana=new Pizza("Mexicana","Pizza sauce, gouda сheese, chicken fillet, red onion, spinach, jalapeno pepper, pepperoni",10.90,"./assets/images/pizzas-section/mexicana.jpg");
+const tonnoeCipolla=new Pizza("Tonno e Cipolla","Pizza sauce, gouda cheese, tuna, red onion",10.90,"./assets/images/pizzas-section/tonno-cipolla.jpg");
+const fiveChesees=new Pizza("Five Chesees","Creamy sauce, gouda cheese, mozzarella cheese, bufallo mozzarella cheese, riccota cheese, parmesan cheese",11.90,"./assets/images/pizzas-section/five-chesees.jpg");
+const volcano=new Pizza("Volcano","Pizza sauce, gouda cheese, spinach, red onion, bell pepper, tomatoes, mushrooms, chicken fillet, olives",11.90,"./assets/images/pizzas-section/volcano.jpg");
+const sujuk=new Pizza("Sujuk","Pizza sauce, gouda сheese, bell pepper, mushrooms, sucuk",11.90,"./assets/images/pizzas-section/sujuk.jpg");
+const amerikana=new Pizza("Amerikana","Barbecue sauce, gouda сheese, sausage, french friez, pickles",11.90,"./assets/images/pizzas-section/amerikana.jpg");
+const fourSeasons=new Pizza("Four Seasons","Meduim - 30 sm, pizza sauce, gouda cheese, white cheese, ham (halal), sucuk, mushrooms, tomatoes, oregano",16.90,"./assets/images/pizzas-section/four-season.jpg");
+const deluxe=new Pizza("Deluxe","Pizza sauce, gouda cheese, red onion, pepperoni pepper, beef",11.90,"./assets/images/pizzas-section/deluxe.jpg");
+const meatLovers=new Pizza("Meat Lovers","Pizza sauce, gouda cheese, chicken fillet, beef, salami (halal), ham (halal), sucuk",12.90,"./assets/images/pizzas-section/meat-lovers.jpg");
+const megaMix=new Pizza("Mega Mix","Pizza sauce, gouda сheese, tomatoes, bell pepper, mushrooms, black olives, salami (halal), ham (halal), beef, chicken fillet",12.90,"./assets/images/pizzas-section/mega-mix.jpg");
+
+
 
 //Adding pizzas to page
 const rowPizza=document.querySelector(".pizza-row");
 pizzas.push(margherita,vegetarian,funghiConPollo,speciale,mista,salami,classico,hawaii,texas,chickenStrips,mexicana,tonnoeCipolla,fiveChesees,volcano,sujuk,amerikana,fourSeasons,deluxe,meatLovers,megaMix);
 localStorage.setItem("pizzas",JSON.stringify(pizzas));
-const showPizzasOnScreen=setInterval(function(){
-    const localPizzas=JSON.parse(localStorage.getItem("pizzas"));
-    console.log("hello");
+const localPizzas=JSON.parse(localStorage.getItem("pizzas"));
     for (let i = 0; i < localPizzas.length; i++) {
         let pizza=localPizzas[i];
         showPizza(pizza);
-    }
-},1000)
+}
 
+//Popup
+const productImages=document.querySelectorAll("#pizzas .product-img");
+const zoomDivs=document.querySelectorAll("#pizzas .icon-zoom");
+const popupImg=document.querySelector("#pizzas .popup .inner img");
+
+for (let i = 0; i < productImages.length; i++) {
+    const productImg=productImages[i];
+    productImg.addEventListener("click",(e)=>{
+        e.preventDefault();
+        popupImg.setAttribute("src",productImg.getAttribute("src"));
+        popup.style.display="block";
+    })
+    
+}
+
+for (let i = 0; i < zoomDivs.length; i++) {
+    const zoomDiv=zoomDivs[i];
+    zoomDiv.addEventListener("click",()=>{
+        const parent=zoomDiv.parentElement;
+        parent.classList.add("zoomImg");
+        const imgZoom=document.querySelector(`.zoomImg img`)
+        parent.classList.remove("zoomImg");
+        popupImg.setAttribute("src",imgZoom.getAttribute("src"));
+        popup.style.display="block";
+        parent.classList.remove("zoomImg");
+    })
+    
+}
+document.addEventListener("click",(e)=>{
+    if (e.target.classList.contains("popup") || e.target.classList.contains("inner")) {
+        popup.style.display="none";
+    }
+});
+
+
+
+
+//Functions
 function showPizza(pizza){
     rowPizza.innerHTML+=`
         <div class="col-3 pizza">
             <div class="card pizza-card" style="width: 14rem;">
-                <img src=${pizza.sourceImg} class="card-img-top" alt=${pizza.pizzaName}>
+                <img src=${pizza.sourceImg} class="card-img-top product-img" alt=${pizza.pizzaName}>
                 <div class="icon-zoom">
                     <i class="fa-solid fa-magnifying-glass zoom"></i>
                 </div>
